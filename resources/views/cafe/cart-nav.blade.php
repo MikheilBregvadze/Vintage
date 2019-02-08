978f360b05c4069288148d67e4493435ef7f88a7
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2019-02-08 18:30:44 +0400

    some changes to view

--- resources/views/cafe/cart-nav.blade.php
+++ resources/views/cafe/cart-nav.blade.php
@@ -3,9 +3,9 @@
     <div class="navbar d-flex nav-border justify-content-between w-100">
         <ul class="d-flex align-items-center">
             <li><a class="circleLink" href="/cafe"><i class="fas fa-home"></i></a></li>
-            <li><a class="linkAfter" href="/cafe">Home</a></li>
+            <li><a style="color: black" href="/cafe">Home</a></li>
             <li><a class="linkAfter" href="/cafe/shop/shop">Shop</a></li>
-            <li><a class=" linkAfterLast" href="javascript:void(0)">Your Card</a></li>
+            <li><a class=" linkAfter" href="javascript:void(0)">Your Card</a></li>
         </ul>
         <div>
             <span class="Item align-items-center"> Item</span>
